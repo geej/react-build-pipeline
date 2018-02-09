d6fadc0e2b3abbd59c1d4f5ae8415c53e5ab9427
geej <jeffrgee@gmail.com>
2018-02-08 22:41:02 -0800

    lock down the lint command, because we have opinions

--- bin/lint.js
+++ bin/lint.js
@@ -3,13 +3,5 @@
 const Path = require('path');
 const exec = require('child_process').exec;
 
-const cwd = process.cwd();
-const entryPoint = process.argv[2];
-
-if (!entryPoint) {
-  console.error('An entry point must be provided');
-  process.exit(1);
-}
-
-const lint = exec(`cd ${ Path.join(__dirname, '..') }; npm run eslint -- ${ Path.join(cwd, entryPoint) } -c ${ Path.join(__dirname, '..', '.eslintrc') }`);
+const lint = exec(`cd ${ Path.join(__dirname, '..') }; npm run eslint -- ${ Path.join(cwd, 'src') } -c ${ Path.join(__dirname, '..', '.eslintrc') }`);
 lint.stdout.on('data', (data) => console.log(data));
