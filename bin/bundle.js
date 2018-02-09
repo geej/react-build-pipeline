eaaffda595c0f1ae3850731da060ddc912e2dc69
geej <me@jeffg.ee>
2018-02-08 21:33:53 -0800

    update scripts to execute in the right directories, using the right config files

--- bin/bundle.js
+++ bin/bundle.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const Path = require('path');
 const exec = require('child_process').exec;
-const lint = exec(`npm run webpack`);
+
+const lint = exec(`cd ${ Path.join(__dirname, '..') }; BASE_PATH=${ process.cwd() } npm run webpack`);
 lint.stdout.on('data', (data) => console.log(data));
--- bin/lint.js
+++ bin/lint.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
+const Path = require('path');
 const exec = require('child_process').exec;
 
+const cwd = process.cwd();
 const entryPoint = process.argv[2];
+
 if (!entryPoint) {
-    console.error('An entry point must be provided');
-    process.exit(1);
+  console.error('An entry point must be provided');
+  process.exit(1);
 }
 
-const lint = exec(`npm run eslint -- ${ entryPoint }`);
+console.log(__dirname);
+const lint = exec(`cd ${ Path.join(__dirname, '..') }; npm run eslint -- ${ Path.join(cwd, entryPoint) } -c ${ Path.join(__dirname, '..', '.eslintrc') }`);
 lint.stdout.on('data', (data) => console.log(data));
