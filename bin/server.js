f1312e140515004b4098491dfb62be98181de85d
geej <me@jeffg.ee>
2018-02-08 21:45:06 -0800

    webpack-dev-server

---
+++ bin/server.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const Path = require('path');
+const exec = require('child_process').exec;
+
+const lint = exec(`cd ${ Path.join(__dirname, '..') }; BASE_PATH=${ process.cwd() } npm run webpack-dev-server`);
+lint.stdout.on('data', (data) => console.log(data));
