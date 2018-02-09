0d3a235b522c621faed942c9ae4dcb82f4a5574f
geej <jeffrgee@gmail.com>
2018-02-08 22:42:03 -0800

    add back in a variable

--- bin/lint.js
+++ bin/lint.js
@@ -3,5 +3,5 @@
 const Path = require('path');
 const exec = require('child_process').exec;
 
-const lint = exec(`cd ${ Path.join(__dirname, '..') }; npm run eslint -- ${ Path.join(cwd, 'src') } -c ${ Path.join(__dirname, '..', '.eslintrc') }`);
+const lint = exec(`cd ${ Path.join(__dirname, '..') }; npm run eslint -- ${ Path.join(process.cwd(), 'src') } -c ${ Path.join(__dirname, '..', '.eslintrc') }`);
 lint.stdout.on('data', (data) => console.log(data));
