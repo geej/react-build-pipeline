0253a3885c42a4c81799b716e153e77582b05ebf
geej <jeffrgee@gmail.com>
2018-02-09 16:53:48 -0800

    add stderr to output

--- bin/bundle.js
+++ bin/bundle.js
@@ -5,3 +5,4 @@ const exec = require('child_process').exec;
 
 const lint = exec(`cd ${ Path.join(__dirname, '..') }; BASE_PATH=${ process.cwd() } npm run webpack`);
 lint.stdout.on('data', (data) => console.log(data));
+lint.stderr.on('data', (data) => console.log(data));
